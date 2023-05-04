adc3089c2f8078074c524cb847f43d6ff8acf3c2
laurabpaulsen <202005791@post.au.dk>
2023-05-04 15:19:15 +0200

    working script

--- topic_model/src/author_topic_index.py
+++ topic_model/src/author_topic_index.py
@@ -9,34 +9,37 @@ NB: all estimates are stored at dictionaries (for JSON export), but written to 2
 import os
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 def main():
-    fname = os.path.join('..','data','theta','diplomats_noretweet_theta_df.csv')
-    df = pd.read_csv(fname)
-    ii = 20
-    colnames = df.columns.values[ii:]
-    theta_dict, index_dict = dict(), dict()
-    m, n = df.shape
-    p_post = list()
-    for (i, usr) in enumerate(sorted(list(set(df['username'].values)))):
-        idx = df['username'] == usr
-        p_post.append(sum(idx)/m*100)
-        print(f'[INFO] processing {i}th handle: {usr}...')        
-        sub_theta_avg = df.loc[idx, :].iloc[:,ii:].values.mean(axis=0)
-        # write to json
-        theta_dict[usr] = sub_theta_avg
-        # index 1: max, 30: min
-        index = np.zeros(sub_theta_avg.shape, dtype=int)
-        index[np.argsort(sub_theta_avg)] = range(20,0,-1)
-        index_dict[usr] = index
+    path = Path(__file__)
+    theta_path = path.parents[1] / "theta"
 
-    # write to tabular
-    df_px = pd.DataFrame.from_dict(theta_dict, orient='index', columns = colnames)
-    df_px['author_presence'] = p_post    
-    df_px.to_csv(os.path.join('..', 'data', 'author_topic_px.csv'), index=True)
-    df_idx = pd.DataFrame.from_dict(index_dict, orient='index', columns = colnames)
-    df_idx['author_presence'] = p_post
-    df_idx.to_excel(os.path.join('..', 'data', 'author_topic_index.xlsx'), index=True)
+    for period, ii in zip(["early", "all", "late"], [20, 30, 35]): 
+        df = pd.read_csv(theta_path / f"diplomats_noretweet_theta_df_{period}.csv")
+        colnames = df.columns.values[ii:]
+        theta_dict, index_dict = dict(), dict()
+        m, n = df.shape
+        p_post = list()
+        for (i, usr) in enumerate(sorted(list(set(df['username'].values)))):
+            idx = df['username'] == usr
+            p_post.append(sum(idx)/m*100)
+            print(f'[INFO] processing {i}th handle: {usr}...')        
+            sub_theta_avg = df.loc[idx, :].iloc[:,ii:].values.mean(axis=0)
+            # write to json
+            theta_dict[usr] = sub_theta_avg
+            # index 1: max, 30: min
+            index = np.zeros(sub_theta_avg.shape, dtype=int)
+            index[np.argsort(sub_theta_avg)] = range(20,0,-1)
+            index_dict[usr] = index
+
+        # write to tabular
+        df_px = pd.DataFrame.from_dict(theta_dict, orient='index', columns = colnames)
+        df_px['author_presence'] = p_post    
+        df_px.to_csv(theta_path / f'author_topic_px_{period}.csv', index=True)
+        df_idx = pd.DataFrame.from_dict(index_dict, orient='index', columns = colnames)
+        df_idx['author_presence'] = p_post
+        df_idx.to_excel(theta_path / f'author_topic_index_{period}.xlsx', index=True)
     
 if __name__=='__main__':
     main()
